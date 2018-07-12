849ad2714e05fac9c64fadb2bb34faeebffc7783
Philip Warner <pjw@rhyme.com.au>
2018-07-12 18:04:59 +1000

    Build 1.2 with Gradle 4.4, Plugin 3.1.3, tools 27.0.3, target 26.
    - no longer supports mips* and armeabi
    - now uses uses c++_static
    - cleanup on build.gradle
    - updated binaries
    
    MAJOR PROBLEM FIXED: and old '.externalNativeBuild' directory was causing breakage

--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -1,4 +1,4 @@
-APP_PLATFORM := android-23
+APP_PLATFORM := android-14
 NDK_TOOLCHAIN_VERSION := clang
-APP_ABI := all
-APP_STL := stlport_static
+APP_ABI := armeabi-v7a, arm64-v8a, x86, x86_64
+APP_STL := c++_static
